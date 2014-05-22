c3e2d0dd180a7e824d286012bd64b4c8c6923cde
ttrfstud <ttrfstud@gmail.com>
2014-05-22 22:50:26 +0300

    init

---
+++ fn/computep.js
@@ -0,0 +1,5 @@
+function computep(jobs) {
+
+}
+
+module.exports = computep;
---
+++ index.js
@@ -0,0 +1,28 @@
+function dpwisp(jobs, weif) {
+  var i, len;
+  var opt;
+  var job;
+  var p;
+  var res;
+
+  opt = [];
+
+  len = jobs.length;
+
+  assert(len > 0);
+
+  opt[0] = 0;
+
+  p = computep(jobs);
+
+  for (i = 1; i < len; i++) {
+    job = jobs[i];
+    opt[i] = Math.max(opt[i - 1], weif(job) + opt[p[i]]);
+  }
+
+  res = traceback(opt);
+
+  return res;
+}
+
+module.exports = dpwisp;
